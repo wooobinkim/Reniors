31e63ffdb17d7861c98b2d3bf96a8b7fb822995c
rladnqsl98 <rladnqls98@gmail.com>
2022-07-25 10:19:14 +0900

    [02_DOCS]backend project commit

---
+++ Back_end/jmark/src/main/java/com/common/jmark/JmarkApplication.java
@@ -0,0 +1,13 @@
+package com.common.jmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmarkApplication.class, args);
+	}
+
+}
---
+++ Back_end/jmark/src/test/java/com/common/jmark/JmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.jmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
