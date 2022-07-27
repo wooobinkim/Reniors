529e3d2ea3e7b9a620bcb56eb406e49d61a14294
석재호 <wogh9705@naver.com>
2022-07-27 20:37:23 +0900

    docs: ✏️ EC2 설정

--- Back_end/jmark/src/main/java/com/common/jmark/controller/TestController.java
+++ Back_end/jmark/src/main/java/com/common/jmark/controller/TestController.java
@@ -1,16 +1,18 @@
 package com.common.jmark.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/")
     public String test(){
-        return "<div>hi<div>";
+        return "<h1>서버 배포 테스트</h1>";
     }
 
 }
