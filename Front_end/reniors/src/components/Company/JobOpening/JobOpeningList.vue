b20c41f52a4d38d1ee069afcf23c2ac50e6d06fe
김우빈 <rladnqls98@gmail.com>
2022-08-03 17:22:49 +0900

    feat: 🎸 Company Component Create

---
+++ Front_end/reniors/src/components/Company/JobOpening/JobOpeningList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>회사 공고페이지</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ Front_end/reniors/src/views/home/CompanyHomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <company-home></company-home>
+  </div>
+</template>
+
+<script>
+import CompanyHome from "@/components/home/CompanyHome.vue";
+export default {
+  components: {
+    CompanyHome,
+  },
+};
+</script>
+
+<style></style>
