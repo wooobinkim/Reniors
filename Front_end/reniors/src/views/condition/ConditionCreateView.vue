efa2fc3f198b19f9711056efc5d8e16c7442c197
jddkgusdlrj <jddkgusdlrj@naver.com>
2022-08-04 17:45:38 +0900

    feat: 🎸 Add Select input boxes

--- Front_end/reniors/src/views/condition/ConditionCreateView.vue
+++ Front_end/reniors/src/views/condition/ConditionCreateView.vue
@@ -1,35 +1,74 @@
 <template>
   <div class="condition-create-view">
     <form action="" @submit.prevent="submit">
-      <button class="condition-create-init"><span>⟳</span> 초기화</button>
+      <button class="condition-create-init" @click="initPayload"><span>⟳</span> 초기화</button>
       <label for="name">맞춤공고 설정</label><br>
-      <input type="text" v-model="name" name="name" id="name">
+      <input type="text" v-model="payload.name" name="name" id="name">
       <hr>
       <label for="region">지역 설정</label><br>
-      <input type="text" v-model="region" name="region" id="region">
+      <select v-model="payload.region" name="region" id="region">
+        <option value="">지역을 선택해주세요.</option>
+        <option v-for="(sido, index) in sidos" :key="index" :value="sido.id">{{ sido.name }}</option>
+      </select>
       <hr>
-      <label for="job">직종 설정</label><br>
-      <input type="text" v-model="job" name="job" id="job">
+      <label for="parent">직종 설정</label><br>
+      <select v-model="payload.parent" name="parent" id="parent" @change="fetchChilds(parent)">
+        <option value="">직무를 선택해주세요.</option>
+        <option v-for="(parent, index) in parents" :key="index" :value="parent.id">{{ parent.name }}</option>
+      </select>
+      <hr>
+      <label for="parent">직종 설정 (세부업무)</label><br>
+      <select v-model="payload.child" name="child" id="child">
+        <option value="">세부업무를 선택해주세요.</option>
+        <option v-for="(child, index) in childs" :key="index" :value="child.id">{{ child.name }}</option>
+      </select>
       <hr>
     </form>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
 export default {
   name: 'ConditionCreateView',
   // props: {
   //   condition: Object,
   // },
   setup() {
-    const submit = () => console.log()
+    const store = useStore()
 
-    let name = ''
-    let region = ''
-    let job = ''
+    const fetchParents = () => store.dispatch('fetchParents')
+    fetchParents()
+    const fetchChilds = (parentId) => store.dispatch('fetchChilds', parentId)
+    const fetchSido = () => store.dispatch('fetchSido')
+    fetchSido()
+    const submit = () => console.log(payload)
+
+    const parents = computed(() => store.getters['parents'])
+    const childs = computed(() => store.getters['childs'])
+    const sidos = computed(() => store.getters['sido'])
+    console.log(sidos)
+
+    // props로 대체 (수정을 위해서)
+    const payload = {
+      name: '',
+      regions: '',
+      parent: '',
+    }
+    const initPayload = () => {
+      payload.name = '',
+      payload.regions = '',
+      payload.parent = '',
+      console.log(payload)
+    }
+    initPayload()
 
     return {
-      submit, name, region, job
+      fetchChilds, submit, initPayload,
+      parents, childs, sidos, 
+      payload,
     }
   }
 }
@@ -54,9 +93,10 @@ export default {
   font-weight: bolder;
 }
 
-.condition-create-view input {
+.condition-create-view input, .condition-create-view select {
   border: 1px solid var(--color-black-3);
   border-radius: 0.4rem;
+  width: 100%;
   padding: 5px 10px;
 }
 </style>
