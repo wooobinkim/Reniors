43e450e97a1b090486748eb54fc711698df8088f
jddkgusdlrj <jddkgusdlrj@naver.com>
2022-08-05 23:46:14 +0900

    fix: 🐛 Fix DetailCondition

--- Front_end/reniors/src/components/jobopening/DetailCondition.vue
+++ Front_end/reniors/src/components/jobopening/DetailCondition.vue
@@ -2,14 +2,14 @@
   <div>
     <div class="jobopening-box">
       <h4>{{ jobopening.title }}</h4>
-      <p>{{ jobopening.companyDto?.name }}</p>
+      <p>{{ '회사이름이 안넘어옴 ㅠㅠ' }}</p>
       <div class="jobopening-box-date">
         <div class="start-badge">시작</div>
-        <p>{{ jobopening.createdDate }}</p>
+        <p>{{ jobopening.createdDate.split('T')[0] }}</p>
       </div>
       <div class="jobopening-box-date">
         <div class="end-badge">종료</div>
-        <p class="end-date">{{ jobopening.finishedDate }}</p>
+        <p class="end-date">{{ jobopening.finishedDate.split('T')[0] }}</p>
       </div>
     </div>
     <div class="jobopening-box">
@@ -24,7 +24,7 @@
       <h3>근무조건</h3>
       <hr>
       <ConditionItem left="급여" :right="jobopening.minSalary + '원'" />
-      <ConditionItem left="지역" right="정보없음" />
+      <ConditionItem left="지역" :right="sidos.find((sido) => sido.id===jobopening?.sidoId)?.name + ' ' + guguns.find((gugun) => gugun.id===jobopening?.gugunId)?.name" />
       <ConditionItem left="근무기간" right="정보없음" />
       <ConditionItem left="근무요일" :right="jobopening.workingDay" />
       <ConditionItem left="근무시간" right="정보없음" />
@@ -32,9 +32,9 @@
     <div class="jobopening-box">
       <h3>모집내용</h3>
       <hr>
-      <ConditionItem left="직책" right="정보없음" />
+      <ConditionItem left="직책" :right="parents.find((parent) => parent.id===jobopening?.jobParentCategory)?.name" />
       <ConditionItem left="직무" :right="jobopening.jobPosition" />
-      <ConditionItem left="고용형태" right="정보없음" />
+      <ConditionItem left="고용형태" :right="jobopening.typeEmployment" />
       <ConditionItem left="모집인원" :right="jobopening.numberPeople" />
     </div>
   </div>
@@ -54,10 +54,20 @@ export default {
     const store = useStore()
     
     const jobopening = computed(() => store.getters['jobopening/selectedJobopening'])
-    const period = jobopening.value.createdDate + ' ~ ' + jobopening.value.finishedDate
+    const period = computed(() => jobopening.value.createdDate.split('T')[0] + ' ~ ' + jobopening.value.finishedDate.split('T')[0])
 
+    const fetchParents = () => store.dispatch('fetchParents')
+    fetchParents()
+    const parents = computed(() => store.getters['parents'])
+
+    const fetchSido = () => store.dispatch('fetchSido')
+    const fetchGugun = (sidoId) => store.dispatch('fetchGugun', sidoId)
+    fetchSido()
+    fetchGugun(jobopening.value.sidoId)
+    const sidos = computed(() => store.getters['sido'])
+    const guguns = computed(() => store.getters['gugun'])
     return {
-      jobopening, period
+      jobopening, period, parents, sidos, guguns,
     }
   },
 }
@@ -84,10 +94,16 @@ export default {
   display: flex;
 }
 
+.jobopening-box-date > p {
+  padding-top: 4px;
+}
+
 .start-badge, .end-badge {
   width: 50px;
   border-radius: 1rem;
   text-align: center;
+  margin: 3px;
+  margin-right: 10px;
 }
 
 .start-badge {
