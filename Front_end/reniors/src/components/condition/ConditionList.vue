06a4cae995ed0f5ce47fe666f2b7c4cecf9f3e45
jddkgusdlrj <jddkgusdlrj@naver.com>
2022-08-06 10:13:32 +0900

    feat: 🎸 Edit ConditionList with splide

--- Front_end/reniors/src/components/condition/ConditionList.vue
+++ Front_end/reniors/src/components/condition/ConditionList.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <hr>
-    <div class="condition-list">
-      <div class="condition-item" v-for="(condition, index) in conditions" :key="index">
+    <Splide class="condition-list" :options="options">
+      <SplideSlide class="condition-item" v-for="(condition, index) in conditions" :key="index">
         <div class="condition-item-header">
           <p class="condition-item-number">맞춤공고{{ index+1 }}</p>
           <div>
@@ -11,49 +11,68 @@
           </div>
         </div>
         <p>{{ condition.region }}</p>
-      </div>
-      <div class="condition-item-create">
+      </SplideSlide>
+      <SplideSlide class="condition-item-create">
         <i>아이콘</i>
         <p>당신의 맞춤 공고를 만들어 보세요 !</p>
-      </div>
-    </div>
+      </SplideSlide>
+    </Splide>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import _ from 'lodash'
 
 export default {
   name: 'ConditionList',
+  components: {
+    Splide, SplideSlide,
+  },
   setup() {
     const store = useStore()
 
     const fetchConditions = () => store.dispatch('condition/fetchConditions')
     fetchConditions()
-    const conditions = computed(() => store.getters['condition/conditions'])
+    const conditions = computed(() => {
+      const datas = store.getters['condition/conditions']
+      if (_.isEmpty(datas?.value)) {
+        return [{ region: '서울' }]
+      }
+      else return datas
+    })
+
+    const options = {
+      padding: 50,
+      arrows: false,
+      pagination: false,
+      wheel: true,
+      wheelSleep: 500,
+      isNavigation: true,
+      width : 360,
+      height : 140,
+      gap : '1rem',
+    }
 
     return {
-      conditions,
+      conditions, options,
     }
   },
 }
 </script>
 
 <style>
-.condition-list {
-  display: flex;
-  flex-wrap: no-wrap;
-  overflow-x: scroll;
+@import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+.splide__track--nav>.splide__list>.splide__slide {
+  border: none;
+  padding: 10px;
 }
 
-.condition-list > div {
-  border-radius: 0.5rem;
-  height: 140px;
-  width: 300px;
-  flex: 0 0 auto;
-  padding: 10px;
-  margin: 5px;
+.splide__track--nav>.splide__list>.splide__slide.is-active {
+  border: none;
 }
 
 .condition-item {
@@ -66,11 +85,15 @@ export default {
   font-size: 12px;
 }
 
+.condition-list li {
+  border-radius: 0.5rem;
+}
+
 .condition-item-create {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  border: 1px dashed black;
+  border: 1px dashed black !important;
   background-color: var(--color-green-4);
   font-weight: bold;
 }
