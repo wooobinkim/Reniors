8ce1aa064382f756194cf73c6fa1561633f80389
jddkgusdlrj <jddkgusdlrj@naver.com>
2022-08-06 18:29:55 +0900

    feat: Edit ConditionResult
    ADD
    - components/condition/ConditionResultItem.vue
    
    EDIT
    - views/condition/ConditionResultView.vue
    - views/condition/ConditionView.vue

--- Front_end/reniors/src/views/condition/ConditionView.vue
+++ Front_end/reniors/src/views/condition/ConditionView.vue
@@ -7,15 +7,13 @@
 </template>
 
 <script>
-import ConditionCreateView from './ConditionCreateView.vue'
-import ConditionResultView from './ConditionResultView.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import ConditionList from '@/components/condition/ConditionList.vue'
 
 export default {
   name: 'ConditionView',
   components: {
-    HeaderComponent, ConditionList, ConditionCreateView, ConditionResultView,
+    HeaderComponent, ConditionList,
   },
 }
 </script>
