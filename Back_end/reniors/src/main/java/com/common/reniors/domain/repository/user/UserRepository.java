92b33168cd7d1e7d8fc9a60cf653a76a30cda791
전병찬 <blackbox98@naver.com>
2022-08-10 18:03:11 +0900

    refactor: 💡 Kakao Login

--- Back_end/reniors/src/main/java/com/common/reniors/domain/repository/user/UserRepository.java
+++ Back_end/reniors/src/main/java/com/common/reniors/domain/repository/user/UserRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserAppId(String userAppID);
-    Optional<User> findByPhone(String phone);
+    Optional<User> findByUserAppId(String userAppId);
+    Optional<User> findByKakaoId(String kakaoId);
     Optional<User> findById(Long Id);
     Optional<User> findByNameAndPhone(String name, String phone);
     Optional<User> findByNameAndUserAppId(String name, String phone);
