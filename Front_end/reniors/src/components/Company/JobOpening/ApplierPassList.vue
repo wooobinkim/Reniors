c97229863c306da51aeb1715526664afb006ba5d
rladnqsl98 <rladnqls98@gmail.com>
2022-08-11 17:59:23 +0900

    feat: 🎸 Company Page Update

--- Front_end/reniors/src/components/Company/JobOpening/ApplierPassList.vue
+++ Front_end/reniors/src/components/Company/JobOpening/ApplierPassList.vue
@@ -1,18 +1,51 @@
 <template>
   <div>
-    <h3>Fin!</h3>
-    <h2>{{ store.state }}</h2>
-    <b-button @click="store.setState('name', 'Carlos')"
-      >Cambiar nombre</b-button
-    >
-    <b-button @click="$emit('set-step', 1)">Ir al paso 1</b-button>
-    <div v-for="(user, i) of store.state.users" :key="i">{{ user }}</div>
+    <template v-for="apply in applylist" :key="apply.id">
+      <template v-if="apply.jobOpeningProcess == '최종합격'">
+        <div>
+          <applier-pass-list-item
+            :apply="apply"
+            :jobOpeningId="this.$route.params.no"
+          >
+          </applier-pass-list-item>
+        </div>
+        <hr>
+      </template>
+    </template>
   </div>
 </template>
 
+<script setup>
+import "@vuepic/vue-datepicker/dist/main.css";
+import { mapActions, mapState } from "vuex";
+</script>
 <script>
+import ApplierPassListItem from "./ApplierInterviewListItem.vue";
 export default {
-  props: ["store"],
+  components: {
+    ApplierPassListItem,
+  },
+  props: {
+    // apply: Object,
+    jobopeningdetail:Object,
+  },
+
+  created(){
+    this.getapplylist(this.$route.params.no);
+  },
+  computed: {
+    ...mapState("company", ["jobopening","applylist"]),
+  },
+  methods: {
+    ...mapActions("company", ["getapplylist", "progressJobOpening","updateApply"]),
+
+    resumeview() {
+      this.$router.push({
+        name: "resumeview",
+        params: { no: this.apply.userId },
+      });
+    },
+  },
 };
 </script>
 
