ddbb77932c53dac427913cf0432e9add012f4f4f
rladnqsl98 <rladnqls98@gmail.com>
2022-08-11 14:59:57 +0900

    feat: 🎸 SrearchCondition response fix

--- Back_end/reniors/src/main/java/com/common/reniors/dto/jobOpening/SearchConditionResponse.java
+++ Back_end/reniors/src/main/java/com/common/reniors/dto/jobOpening/SearchConditionResponse.java
@@ -1,9 +1,13 @@
 package com.common.reniors.dto.jobOpening;
 
+import com.common.reniors.domain.entity.HopeJobChild;
 import com.common.reniors.domain.entity.Type.TypeEmployment;
 import com.common.reniors.domain.entity.Type.LastEdu;
 import com.common.reniors.domain.entity.SearchCondition;
 import lombok.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Setter(AccessLevel.NONE)
@@ -19,6 +23,8 @@ public class SearchConditionResponse {
     private LastEdu lastEdu;
     private Long jobParentCategoryId;
     private String jobParentCategoryName;
+    private List<HopeAreaResponse> hopeAreaResponseList;
+    private List<HopeJobChildResponse> hopeJobChildResponseList;
 
     public static SearchConditionResponse response(SearchCondition searchCondition, String jobParentCategoryName){
         return new SearchConditionResponse(
@@ -30,7 +36,9 @@ public class SearchConditionResponse {
                 searchCondition.getTypeEmployment(),
                 searchCondition.getLastEdu(),
                 searchCondition.getJobParentCategoryId(),
-                jobParentCategoryName
+                jobParentCategoryName,
+                searchCondition.getHopeAreas().stream().map(a->HopeAreaResponse.response(a)).collect(Collectors.toList()),
+                searchCondition.getHopeJobChilds().stream().map(j->HopeJobChildResponse.response(j)).collect(Collectors.toList())
         );
     }
 }
