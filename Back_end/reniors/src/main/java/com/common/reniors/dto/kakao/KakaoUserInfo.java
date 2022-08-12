94a32469fc2ff30e5829489bfe978f2478d45511
전병찬 <blackbox98@naver.com>
2022-08-12 23:37:47 +0900

    refactor: 💡 KakaoLogin Files

--- Back_end/reniors/src/main/java/com/common/reniors/dto/kakao/KakaoUserInfo.java
+++ Back_end/reniors/src/main/java/com/common/reniors/dto/kakao/KakaoUserInfo.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class KakaoUserInfo {
-    private Long id;
     private String nickname;
     private String email;
     private Gender gender;
