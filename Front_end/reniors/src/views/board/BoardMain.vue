edeef2e51f9b0d88c572764ca2e11200198643fa
석재호 <seok@seogjaehoui-MacBookPro.local>
2022-08-17 19:16:11 +0900

    feat: 🎸 board paging

--- Front_end/reniors/src/views/board/BoardMain.vue
+++ Front_end/reniors/src/views/board/BoardMain.vue
@@ -21,6 +21,8 @@
       :key="article.boardId"
       :article="article"
     ></article-item>
+
+    <button v-show="!isLast" class="more-btn" @click="pageUp()">더보기</button>
   </div>
 </template>
 <script>
@@ -42,29 +44,54 @@ export default {
     };
   },
   watch: {
-    $route: function (from) {
-      if (from.path.includes("boards")) {
-        this.fetchArticles(from.params.category_id, 0, 2);
-        this.jobname = this.parents[Number(from.params.category_id) - 1].name;
+    $route: async function (to) {
+      if (to.path.includes("boards")) {
+        await this.setCurrPage(0);
+        await this.clearArticles();
+        await this.fetchArticles({
+          categoryId: this.$route.params.category_id,
+          page: 0,
+        });
+        this.jobname = this.parents[Number(to.params.category_id) - 1].name;
+      }
+    },
+    async currPage() {
+      if (this.currPage != 0) {
+        await this.fetchArticles({
+          categoryId: this.$route.params.category_id,
+          page: this.currPage,
+        });
       }
     },
   },
   computed: {
-    ...mapGetters(["articles", "isLogginedIn", "parents"]),
+    ...mapGetters([
+      "articles",
+      "isLogginedIn",
+      "parents",
+      "isLast",
+      "currPage",
+    ]),
   },
   methods: {
-    ...mapActions(["fetchArticles", "fetchParents"]),
+    ...mapActions([
+      "fetchArticles",
+      "fetchParents",
+      "clearArticles",
+      "setCurrPage",
+    ]),
+    async pageUp() {
+      await this.setCurrPage(this.currPage + 1);
+      console.log("CurrPage : ", this.currPage);
+    },
   },
-  created() {
-    this.fetchParents();
-    console.log("parent");
-    console.log(this.parents[this.$route.params.category_id - 1]);
-    console.log("test log");
-    console.log(this.$route.params.category_id);
-    this.fetchArticles({
+  async created() {
+    await this.setCurrPage(0);
+    await this.fetchParents();
+    await this.clearArticles();
+    await this.fetchArticles({
       categoryId: this.$route.params.category_id,
       page: 0,
-      size: 2,
     });
     this.jobname = this.parents[this.$route.params.category_id - 1].name;
   },
@@ -90,6 +117,16 @@ export default {
   font-weight: bold;
   color: #ffb400;
   margin: 0 16px;
+}
+.more-btn {
+  margin-top: 30px;
+  margin-bottom: 200px;
+  border-radius: 5px;
+  border: none;
+  padding: 8px 40px;
+  background-color: var(--color-red-2);
+  color: white;
+  font-weight: bold;
 }
 @media (max-width: 760px) {
   .board-main-box {
