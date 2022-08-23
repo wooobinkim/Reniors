34f393f1067d94d7edf1c4dca5d70092476746cb
석재호 <wogh9705@naver.com>
2022-08-23 16:39:48 +0900

    fix: 🐛 build error

--- Front_end/reniors/babel.config.js
+++ Front_end/reniors/babel.config.js
@@ -1,10 +1,10 @@
-const removeConsolePlugin =[];
-if(process.env.NODE_ENV === 'production') {
-  removeConsolePlugin.push("transform-remove-console", { "exclude": [ "error", "warn"] })
-}
+// const removeConsolePlugin =[];
+// if(process.env.NODE_ENV === 'production') {
+//   removeConsolePlugin.push("transform-remove-console", { "exclude": [ "error", "warn"] })
+// }
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: removeConsolePlugin
+  //plugins: process.env.NODE_ENV === 'production' ? ['transform-remove-console'] : [],
 }
