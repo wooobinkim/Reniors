90f9a164178136fbb571a56893919320ca73d99a
석재호 <wogh9705@naver.com>
2022-08-23 13:29:28 +0900

    docs: ✏️ npm add

--- Front_end/reniors/babel.config.js
+++ Front_end/reniors/babel.config.js
@@ -1,3 +1,7 @@
+const removeConsolePlugin =[];
+if(process.env.NODE_ENV === 'production') {
+  removeConsolePlugin.push("transform-remove-console", { "exclude": [ "error", "warn"] })
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
